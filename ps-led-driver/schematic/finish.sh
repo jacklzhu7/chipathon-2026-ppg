4c5bfbd53f88c5e53496db73f951677fbb6b10e7
peter-sayegh <132221160+peter-sayegh@users.noreply.github.com>
2026-07-16 21:33:41 -0400

    Remove stray temp file, .spiceinit, and unused script from tracking

--- ps-led-driver/schematic/finish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /foss/designs/sscs-chipathon-2026/led_driver/schematic
-echo "load_schematic {led_driver.sch}
-make_symbol {led_driver.sch}
-exit" | xschem -q -x -s --tcl - 2>&1 | tail -3
-ls -la led_driver.sym 2>&1
